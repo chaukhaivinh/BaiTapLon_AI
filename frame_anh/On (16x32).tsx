7eae3a13c3f94856b5e4d2ac5a45d49fcbf76630
chaukhaivinh <chaukhaivinh1642@gmail.com>
2026-03-22 22:44:12 +0700

    Them do an AI (Bai tap lon)

--- frame_anh/On (16x32).tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="On (16x32)" tilewidth="16" tileheight="16" tilecount="6" columns="3">
- <image source="Free/Traps/Fire/On (16x32).png" width="48" height="32"/>
-</tileset>
